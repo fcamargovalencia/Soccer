df1fff39442ddd95ff270d6c66bf4d4f216981b8
fabian camargo <fabiancv_1025@outlook.com>
2017-10-04 00:24:53 -0500

    Modelo Leagues, API y Backend seguridad de token

---
+++ Soccer.Domain/DataContext.cs
@@ -0,0 +1,14 @@
+﻿namespace Soccer.Domain
+{
+    using System.Data.Entity;
+
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Soccer.Domain.League> Leagues { get; set; }
+    }
+}
